6a34fedf170d4a646ddf3c3efcb6d8ba8bd65dec
Юсуф <122856064+Josef-adilson27@users.noreply.github.com>
2024-10-07 19:05:03 +0300

    2-dice added (ред.)

--- JS-advanced/2-dice.js
+++ JS-advanced/2-dice.js
@@ -1,12 +1,21 @@
+
+
+// Сделать функцию для настольных игр. Которая принимает тип dice, который надо бросить:
+// d4, d6, d8, d10, d12, d16, d20 и возвращает случайное целое число на этом интервале с включёнными границами:
+// d6 - возможные значения - 1, 2, 3, 4, 5, 6
+
+
 const acceptDices = ["d4", "d6", "d8", "d10", "d12", "d16", "d20"];
 
+//получаем рандомную кость
 function getRandomDice() {
-
-  randomDiceNumber = Math.ceil(Math.random() * acceptDices[acceptDices.length - 1].slice(1));
+  randomDiceNumber = Math.ceil(
+    Math.random() * acceptDices[acceptDices.length - 1].slice(1)
+  );
   return "d" + randomDiceNumber;
-  
 }
 
+//получаем значения
 function returnNumber(param) {
   if (acceptDices.includes(param)) {
     const rnd = Number(param.slice(1));
