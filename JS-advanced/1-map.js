749135b1dd88550d2b102cc7c72a58ce19698a4d
Юсуф <122856064+Josef-adilson27@users.noreply.github.com>
2024-10-09 20:57:24 +0300

    отредактировано(1)

--- JS-advanced/1-map.js
+++ JS-advanced/1-map.js
@@ -1,20 +1,15 @@
-let users = [
-  { id: 1, name: "John" },
-  { id: 2, name: "Peter" },
-  { id: 3, name: "Max" },
-  { id: 1, name: "John" },
+const array = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 1, name: "Alice" },
+  { id: 3, name: "Charlie" },
 ];
 
-function uniqueItemsArray(array) { 
-        
-  const set = new Set();
-  const arr = [];
+const uniqueArray = Array.from(new Set(array.map((obj) => obj.id)));
 
-  array.forEach((item) => set.add(JSON.stringify(item)));
-  set.forEach((item) => arr.push(JSON.parse(item)));
+let result = uniqueArray.map((originalObjectId) => {
+  return array.find(obj=>obj.id==originalObjectId)
+});
 
-  return arr;
 
-}
-
-console.log(uniqueItemsArray(users));
+console.log(result);
