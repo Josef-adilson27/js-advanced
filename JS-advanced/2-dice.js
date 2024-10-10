8191d627e783d2354e6bd02b72b8e7864640eb1c
Юсуф <122856064+Josef-adilson27@users.noreply.github.com>
2024-10-10 11:29:42 +0300

    2-dice (отредактировано(2))

--- JS-advanced/2-dice.js
+++ JS-advanced/2-dice.js
@@ -1,30 +1,30 @@
-
-
 // Сделать функцию для настольных игр. Которая принимает тип dice, который надо бросить:
 // d4, d6, d8, d10, d12, d16, d20 и возвращает случайное целое число на этом интервале с включёнными границами:
 // d6 - возможные значения - 1, 2, 3, 4, 5, 6
 
+const acceptDices = {
+  d4: 4,
+  d6: 6,
+  d8: 8,
+  d10: 10,
+  d12: 12,
+  d20: 20,
+};
 
-const acceptDices = ["d4", "d6", "d8", "d10", "d12", "d16", "d20"];
-
-//получаем рандомную кость
 function getRandomDice() {
-  randomDiceNumber = Math.ceil(
-    Math.random() * acceptDices[acceptDices.length - 1].slice(1)
-  );
-  return "d" + randomDiceNumber;
+  const diceKeys = Object.keys(acceptDices);
+  const randomDiceIndex = Math.floor(Math.random() * diceKeys.length - 1);
+  return diceKeys[randomDiceIndex];
 }
 
-//получаем значения
 function returnNumber(param) {
-  if (acceptDices.includes(param)) {
-    const rnd = Number(param.slice(1));
-    return Math.ceil(Math.random() * rnd);
-  } else {
-    return `уппс... увы кость ${param} неправильная.
-      Вам доступны следующие значения: ${[...acceptDices]}
-    `;
-  }
+  return param in acceptDices
+    ? Math.ceil(Math.random() * acceptDices[param])
+    : `Упс... увы, кость "${param}" неправильная. Вам доступны следующие значения: ${Object.keys(
+        acceptDices
+      ).join(", ")}`;
 }
 
-console.log(returnNumber(getRandomDice()));
+const randomDice = getRandomDice();
+
+console.log(returnNumber(randomDice));
