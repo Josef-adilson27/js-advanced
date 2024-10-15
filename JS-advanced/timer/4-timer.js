0f43fbd1972779294a75cfde05da1c6e3c377736
Юсуф <122856064+Josef-adilson27@users.noreply.github.com>
2024-10-15 18:36:25 +0300

    4-timer has added

---
+++ JS-advanced/timer/4-timer.js
@@ -0,0 +1,35 @@
+let text = document.querySelector('.timer-box')
+
+const newYearDate = new Date(new Date().getFullYear() + 1, 0, 1); 
+
+
+function updateTimer() {
+
+    const now = new Date();
+
+    const totalSeconds = Math.floor((newYearDate - now) / 1000);
+    
+    const months = Math.floor(totalSeconds / (30 * 24 * 60 * 60)); 
+    const days = Math.floor((totalSeconds % (30 * 24 * 60 * 60)) / (24 * 60 * 60));
+    const hours = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60));
+    const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+    const seconds = totalSeconds % 60;
+
+    const rtf = new Intl.NumberFormat('ru', { numeric: 'auto' });
+
+    const dates = [
+        rtf.format(months, 'month'),
+        rtf.format(days, 'day'),
+        rtf.format(hours, 'hour'),
+        rtf.format(minutes, 'minute'),
+        rtf.format(seconds, 'second')
+    ]
+    
+    for (let index = 0; index < text.children.length; index++) {
+        text.children[index].firstElementChild.innerHTML = dates[index] 
+    }
+}
+
+setInterval(updateTimer, 1000);
+
+
